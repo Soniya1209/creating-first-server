c6c7293017256f6e57fa926a1d3ab802270c8cbd
Soniya1209 <soniyapatidar2016@gmail.com>
2024-04-05 18:24:25 +0530

    completed the code for node.js server

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const port = 8081;
+
+http
+    .createServer((require, response) => {
+        response.writeHead(200, {'Conten-Type': 'text/html'});
+        response.write("<h1>hello! This is from my server</h1>");
+        response.end();
+    })
+    .listen(port, () => {
+    console.log(`Node.js server started on port ${port}`);
+    });
+
+    // http://localhost:8081
