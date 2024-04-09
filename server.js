0072e92fe3e2f960075301a36570213300905681
Soniya1209 <soniyapatidar2016@gmail.com>
2024-04-09 14:32:58 +0530

    completed GET and POST methods

--- server.js
+++ server.js
@@ -2,14 +2,34 @@ const http = require('http');
 
 const port = 8081;
 
+const toDoList = ["Complete Node Byte", "Play Cricket"];
+
 http
-    .createServer((require, response) => {
-        response.writeHead(200, {'Conten-Type': 'text/html'});
-        response.write("<h1>hello! This is from my server</h1>");
-        response.end();
+    .createServer((req, res) => {
+        const {method, url} = req; //destructuring of request object
+        if(url === "/todos"){
+            if(method === "GET"){
+                res.writeHead(200,{"Content-Type":"text/html"});
+                res.write(toDoList.toString());             //converting array to string
+            }else if(method === 'POST'){
+                let body = ""; //variable to store data 
+                req.on('error',(err) => {
+                    console.error(err);
+                }).on('data', (chunk) => {
+                    body += chunk;
+                }).on('end', () => {
+                    body = JSON.parse(body);
+                    console.log("data : ",body);
+                })
+            }
+            else{
+                res.writeHead(501);
+            }
+        }
+        res.end();
     })
     .listen(port, () => {
     console.log(`Node.js server started on port ${port}`);
     });
 
-    // npm rum
+    
